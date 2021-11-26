675883f6e59c24b1ef397e8e8e3d1763d840d807
Casi <19554049+GatdeSoia@users.noreply.github.com>
2021-11-26 14:28:22 +0100

    commented out "disable right click" script
    keeping it up for documentation reasons

--- js/script.js
+++ js/script.js
@@ -1,25 +1,26 @@
+//////// [COMMENT] This script affected the whole page. Removes accessibility. Only saving for documenting purposes.
 //// Disable RIGHT CLICK on video tag
-window.onload = function() {
-  document.addEventListener("contextmenu", function(e){
-    e.preventDefault();
-  }, false);
-  document.addEventListener("keydown", function(e) {
-  //document.onkeydown = function(e) {
-  // "I" key / "J" key / "S" key + macOS / "U" key
-    if ((e.ctrlKey && e.shiftKey && e.keyCode == 73) || (e.ctrlKey && e.shiftKey && e.keyCode == 74) || (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) || (e.ctrlKey && e.keyCode == 85) || (event.keyCode == 123)) {
-      disabledEvent(e);
-    }
-  }, false);
-  function disabledEvent(e){
-    if (e.stopPropagation){
-      e.stopPropagation();
-    } else if (window.event){
-      window.event.cancelBubble = true;
-    }
-    e.preventDefault();
-    return false;
-  }
-};
+// window.onload = function() {
+//   document.addEventListener("contextmenu", function(e){
+//     e.preventDefault();
+//   }, false);
+//   document.addEventListener("keydown", function(e) {
+//   //document.onkeydown = function(e) {
+//   // "I" key / "J" key / "S" key + macOS / "U" key
+//     if ((e.ctrlKey && e.shiftKey && e.keyCode == 73) || (e.ctrlKey && e.shiftKey && e.keyCode == 74) || (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) || (e.ctrlKey && e.keyCode == 85) || (event.keyCode == 123)) {
+//       disabledEvent(e);
+//     }
+//   }, false);
+//   function disabledEvent(e){
+//     if (e.stopPropagation){
+//       e.stopPropagation();
+//     } else if (window.event){
+//       window.event.cancelBubble = true;
+//     }
+//     e.preventDefault();
+//     return false;
+//   }
+// };
 
 // //// YT API
 // // Load the IFrame Player API code asynchronously.
